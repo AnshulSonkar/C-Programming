d0194ffadeb4aaaf0222c0b23b0378c19bf3dbf0
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 00:32:05 +0530

    StrCat without function

--- lec18/Strcat_demo.C
+++ lec18/Strcat_demo.C
@@ -1,12 +1,38 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+
+//     char str1[50] = "Hello ";
+//     char str2[] = "World";
+
+//     strcat(str1, str2);
+
+//     printf("%s", str1);
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
-#include <string.h>
 
 int main() {
 
-    char str1[50] = "Hello ";
+    char str1[50] = "Hello";
     char str2[] = "World";
 
-    strcat(str1, str2);
+    int i = 0, j = 0;
+
+    while(str1[i] != '\0')
+        i++;
+
+    while(str2[j] != '\0') {
+        str1[i] = str2[j];
+        i++;
+        j++;
+    }
+
+    str1[i] = '\0';
 
     printf("%s", str1);
 
