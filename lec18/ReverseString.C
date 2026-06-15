2f54d2c23c9c804db05e7214890b7152f0fc209e
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 00:39:37 +0530

    Palindrome  string

--- lec18/ReverseString.C
+++ lec18/ReverseString.C
@@ -1,24 +1,57 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+
+//     char str[] = "HELLO";
+
+//     int start = 0;
+//     int end = strlen(str) - 1;
+
+//     while(start < end) {
+
+//         char temp = str[start];
+//         str[start] = str[end];
+//         str[end] = temp;
+
+//         start++;
+//         end--;
+//     }
+
+//     printf("%s", str);
+
+//     return 0;
+// }
+
+
+// Palindrome String
+
 #include <stdio.h>
 #include <string.h>
 
 int main() {
 
-    char str[] = "HELLO";
+    char str[] = "madam";
 
     int start = 0;
     int end = strlen(str) - 1;
+    int flag = 1;
 
     while(start < end) {
 
-        char temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
+        if(str[start] != str[end]) {
+            flag = 0;
+            break;
+        }
 
         start++;
         end--;
     }
 
-    printf("%s", str);
+    if(flag)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
 
     return 0;
 }
