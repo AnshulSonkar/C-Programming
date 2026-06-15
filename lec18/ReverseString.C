75bb3a261a5b5f0354f2c567a9e6a0ac03a29e0c
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 00:44:35 +0530

    Count Vowels

--- lec18/ReverseString.C
+++ lec18/ReverseString.C
@@ -26,32 +26,58 @@
 
 // Palindrome String
 
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+
+//     char str[] = "madam";
+
+//     int start = 0;
+//     int end = strlen(str) - 1;
+//     int flag = 1;
+
+//     while(start < end) {
+
+//         if(str[start] != str[end]) {
+//             flag = 0;
+//             break;
+//         }
+
+//         start++;
+//         end--;
+//     }
+
+//     if(flag)
+//         printf("Palindrome");
+//     else
+//         printf("Not Palindrome");
+
+//     return 0;
+// }
+
+
+// Count vowels 
+
 #include <stdio.h>
-#include <string.h>
 
 int main() {
 
-    char str[] = "madam";
+    char str[] = "Programming";
+    int count = 0;
 
-    int start = 0;
-    int end = strlen(str) - 1;
-    int flag = 1;
+    for(int i = 0; str[i] != '\0'; i++) {
 
-    while(start < end) {
-
-        if(str[start] != str[end]) {
-            flag = 0;
-            break;
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' ||
+           str[i]=='o' || str[i]=='u' || str[i]=='A' ||
+           str[i]=='E' || str[i]=='I' || str[i]=='O' ||
+           str[i]=='U')
+        {
+            count++;
         }
-
-        start++;
-        end--;
     }
 
-    if(flag)
-        printf("Palindrome");
-    else
-        printf("Not Palindrome");
+    printf("Vowels = %d", count);
 
     return 0;
 }
