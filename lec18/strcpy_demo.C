0ce8468277d05baf30ace4dbcd69d5c0fce677d1
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 00:29:09 +0530

    Strcpy

--- lec18/strcpy_demo.C
+++ lec18/strcpy_demo.C
@@ -1,12 +1,34 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+
+//     char str1[] = "C Language";
+//     char str2[50];
+
+//     strcpy(str2, str1);
+
+//     printf("%s", str2);
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
-#include <string.h>
 
 int main() {
 
-    char str1[] = "C Language";
-    char str2[50];
+    char str1[] = "Hello";
+    char str2[20];
 
-    strcpy(str2, str1);
+    int i = 0;
+
+    while(str1[i] != '\0') {
+        str2[i] = str1[i];
+        i++;
+    }
+
+    str2[i] = '\0';
 
     printf("%s", str2);
 
