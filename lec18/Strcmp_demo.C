1001f84f952677816580a3a1a2b222e9c50ba624
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 00:36:00 +0530

    Strcmp Different strings

--- lec18/Strcmp_demo.C
+++ lec18/Strcmp_demo.C
@@ -1,15 +1,31 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+
+//     char str1[] = "Apple";
+//     char str2[] = "Apple";
+
+//     if(strcmp(str1, str2) == 0)
+//         printf("Strings are Equal");
+//     else
+//         printf("Strings are Not Equal");
+
+//     return 0;
+// }  
+
+
 #include <stdio.h>
 #include <string.h>
 
 int main() {
 
     char str1[] = "Apple";
-    char str2[] = "Apple";
+    char str2[] = "Mango";
 
-    if(strcmp(str1, str2) == 0)
-        printf("Strings are Equal");
-    else
-        printf("Strings are Not Equal");
+    int result = strcmp(str1, str2);
+
+    printf("%d", result);
 
     return 0;
 }
