35af0f7bb005edc748c688bff0d86a6c1f1b7553
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 23:23:05 +0530

    Upper triangular Matrix

--- lec19/IdentityMatrix.C
+++ lec19/IdentityMatrix.C
@@ -1,9 +1,42 @@
+// #include <stdio.h>
+
+// int main() {
+
+//     int a[3][3];
+//     int flag=1;
+
+//     for(int i=0;i<3;i++) {
+//         for(int j=0;j<3;j++) {
+//             scanf("%d",&a[i][j]);
+//         }
+//     }
+
+//     for(int i=0;i<3;i++) {
+//         for(int j=0;j<3;j++) {
+
+//             if(i==j && a[i][j]!=1)
+//                 flag=0;
+
+//             if(i!=j && a[i][j]!=0)
+//                 flag=0;
+//         }
+//     }
+
+//     if(flag)
+//         printf("Identity Matrix");
+//     else
+//         printf("Not Identity Matrix");
+
+//     return 0;
+// }
+
+
+// Upper Triangular Matrix
 #include <stdio.h>
 
 int main() {
 
     int a[3][3];
-    int flag=1;
 
     for(int i=0;i<3;i++) {
         for(int j=0;j<3;j++) {
@@ -14,18 +47,14 @@ int main() {
     for(int i=0;i<3;i++) {
         for(int j=0;j<3;j++) {
 
-            if(i==j && a[i][j]!=1)
-                flag=0;
-
-            if(i!=j && a[i][j]!=0)
-                flag=0;
+            if(i<=j)
+                printf("%d ",a[i][j]);
+            else
+                printf("0 ");
         }
-    }
 
-    if(flag)
-        printf("Identity Matrix");
-    else
-        printf("Not Identity Matrix");
+        printf("\n");
+    }
 
     return 0;
 }
