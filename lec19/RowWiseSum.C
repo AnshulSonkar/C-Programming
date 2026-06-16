85c4070dea2c7999c78ab5809756cd71eb615d01
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 13:27:38 +0530

    Column Wis Sum

--- lec19/RowWiseSum.C
+++ lec19/RowWiseSum.C
@@ -1,25 +1,25 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
+// int main() {
 
-    int a[3][3];
+//     int a[3][3];
 
-    for(int i=0;i<3;i++) {
-        for(int j=0;j<3;j++) {
-            scanf("%d",&a[i][j]);
-        }
-    }
+//     for(int i=0;i<3;i++) {
+//         for(int j=0;j<3;j++) {
+//             scanf("%d",&a[i][j]);
+//         }
+//     }
 
-    for(int i=0;i<3;i++) {
+//     for(int i=0;i<3;i++) {
 
-        int sum=0;
+//         int sum=0;
 
-        for(int j=0;j<3;j++) {
-            sum+=a[i][j];
-        }
+//         for(int j=0;j<3;j++) {
+//             sum+=a[i][j];
+//         }
 
-        printf("Row %d Sum = %d\n",i+1,sum);
-    }
+//         printf("Row %d Sum = %d\n",i+1,sum);
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
