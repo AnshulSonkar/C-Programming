1781be5f8d377662af781d31b59162b78aff603b
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 13:05:32 +0530

    Matrix Subtraction

---
+++ lec19/Matrixsubtract.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int a[2][2]={{10,20},{30,40}};
+    int b[2][2]={{5,5},{5,5}};
+    int c[2][2];
+
+    for(int i=0;i<2;i++) {
+        for(int j=0;j<2;j++) {
+            c[i][j]=a[i][j]-b[i][j];
+        }
+    }
+
+    for(int i=0;i<2;i++) {
+        for(int j=0;j<2;j++) {
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
