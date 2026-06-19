9924a67abb5de5079f13dbf61dd305a4ed47cdfe
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-19 22:37:39 +0530

    Dynamic Memory Allocation

--- DMA/Mallocdemo.C
+++ DMA/Mallocdemo.C
@@ -1,21 +1,48 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main() {
+
+//     int *ptr;
+
+//     ptr = (int *)malloc(5 * sizeof(int));
+
+//     for(int i = 0; i < 5; i++) {
+//         ptr[i] = i + 1;
+//     }
+
+//     for(int i = 0; i < 5; i++) {
+//         printf("%d ", ptr[i]);
+//     }
+
+//     free(ptr);
+
+//     return 0;
+// } 
+
+
+// User Input Array Using malloc()
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
 
-    int *ptr;
+    int n;
 
-    ptr = (int *)malloc(5 * sizeof(int));
+    printf("Enter Size: ");
+    scanf("%d", &n);
 
-    for(int i = 0; i < 5; i++) {
-        ptr[i] = i + 1;
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    for(int i = 0; i < 5; i++) {
-        printf("%d ", ptr[i]);
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
 
-    free(ptr);
+    free(arr);
 
     return 0;
 }
