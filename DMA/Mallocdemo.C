b921c8dd65d9fddd63f0567cf254383f35a73ac5
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-19 22:44:14 +0530

    Dynamic Memory Allocation

--- DMA/Mallocdemo.C
+++ DMA/Mallocdemo.C
@@ -69,6 +69,35 @@
 
 
 // realloc()
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main() {
+
+//     int *ptr;
+
+//     ptr = (int *)malloc(3 * sizeof(int));
+
+//     ptr[0] = 10;
+//     ptr[1] = 20;
+//     ptr[2] = 30;
+
+//     ptr = (int *)realloc(ptr, 5 * sizeof(int));
+
+//     ptr[3] = 40;
+//     ptr[4] = 50;
+
+//     for(int i = 0; i < 5; i++) {
+//         printf("%d ", ptr[i]);
+//     }
+
+//     free(ptr);
+
+//     return 0; 
+// } 
+
+
+// Free()
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -76,20 +105,7 @@ int main() {
 
     int *ptr;
 
-    ptr = (int *)malloc(3 * sizeof(int));
-
-    ptr[0] = 10;
-    ptr[1] = 20;
-    ptr[2] = 30;
-
-    ptr = (int *)realloc(ptr, 5 * sizeof(int));
-
-    ptr[3] = 40;
-    ptr[4] = 50;
-
-    for(int i = 0; i < 5; i++) {
-        printf("%d ", ptr[i]);
-    }
+    ptr = (int *)malloc(5 * sizeof(int));
 
     free(ptr);
 
