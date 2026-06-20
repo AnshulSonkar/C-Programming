e63295b58ef0a6b94c9efe0b895642633cdeff79
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-20 23:46:13 +0530

    Recursion

--- Recursion/Recursion_intro.C
+++ Recursion/Recursion_intro.C
@@ -40,21 +40,40 @@
 // } 
 
 
+// #include <stdio.h>
+
+// void print(int n) {
+
+//     if(n == 0)
+//         return;
+
+//     printf("%d ", n);
+
+//     print(n - 1);
+// }
+
+// int main() {
+
+//     print(5);
+
+//     return 0;
+// } 
+
+
+// Fcatorial Using Recurion 
 #include <stdio.h>
 
-void print(int n) {
+int factorial(int n) {
 
-    if(n == 0)
-        return;
+    if(n == 0 || n == 1)
+        return 1;
 
-    printf("%d ", n);
-
-    print(n - 1);
+    return n * factorial(n - 1);
 }
 
 int main() {
 
-    print(5);
+    printf("%d", factorial(5));
 
     return 0;
 }
