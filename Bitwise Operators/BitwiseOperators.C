10ebda780e99828ac5d4e3d2476a3a3bbd9ffde9
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-23 21:07:17 +0530

    Bitwise Operators

--- Bitwise Operators/BitwiseOperators.C
+++ Bitwise Operators/BitwiseOperators.C
@@ -41,13 +41,26 @@
 
 
 // Bitwise NOT
+// #include <stdio.h>
+
+// int main() {
+
+//     int a = 5;
+
+//     printf("%d", ~a);
+
+//     return 0;
+// } 
+
+
+// Left shift <<
 #include <stdio.h>
 
 int main() {
 
     int a = 5;
 
-    printf("%d", ~a);
+    printf("%d", a << 1);
 
     return 0;
 }
