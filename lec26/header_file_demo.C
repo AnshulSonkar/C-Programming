c47618bfd0f0b87529d8d4c433367d07a41fb453
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-23 21:42:22 +0530

    Preprocessor Directives

--- lec26/header_file_demo.C
+++ lec26/header_file_demo.C
@@ -2,4 +2,17 @@
 
 // int add(int a, int b) {
 //     return a + b;
-// }
+// } 
+
+
+// main.c file 
+// #include <stdio.h>
+// #include "myheader.h"
+
+// int main() {
+
+//     printf("%d\n", add(10,20));
+//     printf("%.2f", PI);
+
+//     return 0;
+// } 
